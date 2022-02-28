61d2a9c4a2532c58e558aae5ad05e4ee9a548f0b
andre.stender <andre.stender@netgroup.com>
2022-02-28 02:28:03 +0200

    Swagger

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -44,11 +44,8 @@ builder.Services.AddCoreServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
